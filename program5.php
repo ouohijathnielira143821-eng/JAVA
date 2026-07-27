01871c6208db65611d87f74cb21673c169b64db8
ouohijathnielira143821-eng <ouohijathnielira.143821@marwadiuniversity.ac.in>
2026-07-27 15:44:21 +0530

    Add files via upload

---
+++ program5.php
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 80) {
+            System.out.println("Grade B");
+        } else if (marks >= 70) {
+            System.out.println("Grade C");
+        } else if (marks >= 60) {
+            System.out.println("Grade D");
+        } else if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+        sc.close();
+    }
+}
