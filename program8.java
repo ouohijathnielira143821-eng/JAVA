72d3b9a2da04aaa7833a6c4dbf65f23c5fea9c13
ouohijathnielira143821-eng <ouohijathnielira.143821@marwadiuniversity.ac.in>
2026-07-27 15:46:10 +0530

    Add files via upload

---
+++ program8.java
@@ -0,0 +1,12 @@
+public class Q8 {
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(args[0]);
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
