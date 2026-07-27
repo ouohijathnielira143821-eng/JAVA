64396c69fb5d3585ee2ff65dd58066fb3dd61a3e
ouohijathnielira143821-eng <ouohijathnielira.143821@marwadiuniversity.ac.in>
2026-07-27 15:47:52 +0530

    Add files via upload

---
+++ program10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
